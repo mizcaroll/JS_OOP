569026b94dee68c0fb3fdb39c353a0c676dc9716
mizcaroll <carolomukule@gmail.com>
2021-03-25 14:46:31 +0300

    Factory function

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 
-//object literal definition
+//Factory function
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function() {
+     console.log('draw');
+   }
+  };
+}
+const circle = createCircle(1)
+circle.draw(); 
+/* 
 
+//object literal syntax
  const circle = {
    radius: 1,
    location: {
@@ -11,4 +23,4 @@
      console.log('draw');
    }
  };
- circle.draw();
+ circle.draw(); */
